de44a0916ed58eb30f07e2a5b5bd6fc662755bb8
irwanbc79 <irwanbc79@users.noreply.github.com>
2026-03-25 13:21:53 +0700

    fix: align all signature columns to same baseline on surat jalan

--- resources/views/admin/print-do.blade.php
+++ resources/views/admin/print-do.blade.php
@@ -149,16 +149,16 @@
     </table>
 
     <div class="grid grid-cols-4 gap-4 text-center mt-12">
-        <div>
+        <div class="flex flex-col">
             <p class="text-xs font-bold mb-2">ADMIN / PENGIRIM</p>
-            <div class="flex justify-center mb-1">
+            <div class="flex-1 flex items-end justify-center mb-1">
                 <img src="{{ asset('images/assets/signatures/stamp_m2b.png') }}" class="h-28 w-auto object-contain">
             </div>
             <p class="border-t border-black mx-2 pt-1 font-bold">( PT. M2B )</p>
         </div>
-        <div><p class="text-xs font-bold mb-16">SUPIR / CARRIER</p><p class="border-t border-black mx-2 pt-1">( {{ $container['nama_supir'] ?: '.................' }} )</p></div>
-        <div><p class="text-xs font-bold mb-16">SECURITY GUDANG</p><p class="border-t border-black mx-2 pt-1">( ................. )</p></div>
-        <div><p class="text-xs font-bold mb-16">PENERIMA BARANG</p><p class="border-t border-black mx-2 pt-1">( ................. )</p></div>
+        <div class="flex flex-col"><p class="text-xs font-bold mb-2">SUPIR / CARRIER</p><div class="flex-1"></div><p class="border-t border-black mx-2 pt-1">( {{ $container['nama_supir'] ?: '.................' }} )</p></div>
+        <div class="flex flex-col"><p class="text-xs font-bold mb-2">SECURITY GUDANG</p><div class="flex-1"></div><p class="border-t border-black mx-2 pt-1">( ................. )</p></div>
+        <div class="flex flex-col"><p class="text-xs font-bold mb-2">PENERIMA BARANG</p><div class="flex-1"></div><p class="border-t border-black mx-2 pt-1">( ................. )</p></div>
     </div>
 
     {{-- Footer Info --}}
